f39dc3e761484be5f2ea065729f4212f5125c22c
rex644 <jeff@jeffcomputers.com>
2014-08-13 23:23:33 -0500

    Update tunnel.lua

--- tunnel.lua
+++ tunnel.lua
@@ -4,29 +4,29 @@
 --modem.open(159)
 
 while true do
-sleep(0.1)
-term.clear()
-term.setCursorPos(1,1)
-print("Listening on port 80...")
-local id, msg, distance = rednet.receive()
-print("We've got a message from computer ID #"..id.."!")
-print("The message is:")
-print(msg)
-print("This message was received from "..distance.." blocks away!")
-if msg == "forward" then
-turtle.forward()
-elseif msg == "back" then
-turtle.back()
-elseif msg == "dig" then
-turtle.dig()
-elseif msg == "right" then
-turtle.turnRight()
-elseif msg == "left" then
-turtle.turnLeft()
-elseif msg == "down" then
-turtle.down()
-elseif msg == "up" then
-turtle.up()
-elseif msg == "exit" then
-error()
+  sleep(0.1)
+  term.clear()
+  term.setCursorPos(1,1)
+  print("Listening on port 80...")
+  local id, msg, distance = rednet.receive()
+  print("We've got a message from computer ID #"..id.."!")
+  print("The message is:")
+  print(msg)
+  print("This message was received from "..distance.." blocks away!")
+  if msg == "forward" then
+    turtle.forward()
+  elseif msg == "back" then
+    turtle.back()
+  elseif msg == "dig" then
+    turtle.dig()
+  elseif msg == "right" then
+    turtle.turnRight()
+  elseif msg == "left" then
+    turtle.turnLeft()
+  elseif msg == "down" then
+    turtle.down()
+  elseif msg == "up" then
+    turtle.up()
+  elseif msg == "exit" then
+    error()
 end

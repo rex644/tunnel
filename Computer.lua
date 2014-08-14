3b5c3215c2d8968967c4675bf11afd5178b8d1d3
rex644 <jeff@jeffcomputers.com>
2014-08-13 23:21:13 -0500

    Create Computer.lua

---
+++ Computer.lua
@@ -0,0 +1,9 @@
+print("Type 'forward' to move turtle forward, 'back' to move turtle back, etc.")
+while true do
+sleep(0.1)
+term.clear()
+term.setCursorPos(1,1)
+write("Command: ")
+input = read()
+rednet.broadcast(input)
+end
